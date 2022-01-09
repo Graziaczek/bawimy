aecd061495b98e107e135b4d0fa9d76a495349fe
Graziaczek <66276396+Graziaczek@users.noreply.github.com>
2022-01-09 22:10:55 +0100

    Update ConsoleApplication1.cpp

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main()
 {
     cout << "hej Graziaaaa, dorzuc tu pare linijek od siebie ;)" << endl;
+    cout << "Siemano Konrad, juz wrzucam nowy kodzik ;P" << endl;
     system("pause");
 }
 
